3456ae97add0df1ac6d190dfaae59eb6330daae3
emmie0 <117661780+emmie0@users.noreply.github.com>
2025-04-15 10:43:25 +0800

    j

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './i18n/i18n'; // энд i18n-г холбож өгнө
+import './i18n'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
